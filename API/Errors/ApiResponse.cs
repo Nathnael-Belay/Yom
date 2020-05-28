9d4470d6d8ef3bbf7f012f8a9e111fc33242b7c6
Nathnael <nathnaelbelay21@gmail.com>
2020-05-28 10:15:41 +0300

    added error and exception handler in the api

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            this.StatusCode = statusCode;
+            this.Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Bad request",
+                401 => "Unauthorized request",
+                404 => "Resource not found",
+                500 => "Internal server error",
+                _ => null
+            };
+        }
+    }
+}
